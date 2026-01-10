98e2ca312611886e46b6f2c38002d99a8064e837
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-01-10 22:34:49 +0800

    add+edit users

--- dnc_frontend/src/app/api_services/user-service.ts
+++ dnc_frontend/src/app/api_services/user-service.ts
@@ -20,6 +20,12 @@ export interface User{
   last_modified_by: string;
   last_modified_on: Date;
 }
+export interface NewOrPatchUser{
+  name: string;
+  password?: string;
+  email: string;
+  role_id: number;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -31,6 +37,17 @@ export class UserService {
     let token = this.LoginService.token();
     const headers = {'Authorization': `Bearer ${token}`};
     return this.http.get<UserPageInfo>(`${this.apiUrl}/api/users?`, {headers});
+  }
+  postUser(newUser:NewOrPatchUser){
+    let token = this.LoginService.token();
+    const headers = {'Authorization': `Bearer ${token}`};
+    console.log("In User Service, Post User:", newUser);
+    return this.http.post<User>(`${this.apiUrl}/api/users/`, newUser, {headers});
+  }
+  patchUser(userId:number, newUser:NewOrPatchUser){
+    let token = this.LoginService.token();
+    const headers = {'Authorization': `Bearer ${token}`};
+    return this.http.patch<User>(`${this.apiUrl}/api/users/${userId}`, newUser, {headers});
   }
 
 }
