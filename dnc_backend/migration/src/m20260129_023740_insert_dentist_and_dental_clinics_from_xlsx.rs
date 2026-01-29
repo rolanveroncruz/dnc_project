6b2c6cb24471c20638c9d832909c0adc7fc4b983
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-01-30 03:04:52 +0800

    dentist, clinics, and dentist_clinics imported.

--- dnc_backend/migration/src/m20260129_023740_insert_dentist_and_dental_clinics_from_xlsx.rs
+++ dnc_backend/migration/src/m20260129_023740_insert_dentist_and_dental_clinics_from_xlsx.rs
@@ -1,13 +1,19 @@
 use sea_orm_migration::{prelude::*};
 use calamine::{open_workbook_from_rs, Data, Reader, Xlsx};
 use std::io::Cursor;
+use crate::m20260126_063012_create_tables_dental_clinic::{City, DentalClinic, Region};
+use crate::m20260126_063012_create_tables_dental_clinic::Province;
+use sea_query::{Expr, OnConflict, Query};
+use crate::m20260119_112338_create_table_dentist_contract::DentistContract;
+use crate::m20260126_161604_create_table_dentists::{Dentist, DentistClinic};
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
-        Self::insert_dentists_and_clinics_from_xlsx(_manager)?;
+        Self::insert_dentists_and_clinics_from_xlsx(_manager).await?;
         Ok(())
     }
 
@@ -19,7 +25,7 @@ impl MigrationTrait for Migration {
 #[derive(Debug)]
 struct DentistRow{
     contract: String,
-    first_name: String,
+    given_name: String,
     middle_name: String,
     last_name: String,
     clinic_name: String,
@@ -28,10 +34,12 @@ struct DentistRow{
     clinic_province: String,
     clinic_region: String,
     clinic_zip: String,
-    clinic_contact: String,
+    clinic_contact_numbers: String,
     clinic_schedule: String,
     dentist_position: String,
+    clinic_email: String,
 }
+
 impl DentistRow{
     fn from_row( row: &[Data])->Option<Self>{
         let get = |i:usize| row.get(i).map(cell_to_string).unwrap_or_default();
@@ -41,7 +49,7 @@ impl DentistRow{
         }
         let dentist = Self{
             contract: get(0),
-            first_name: get(2),
+            given_name: get(2),
             middle_name: get(3),
             last_name:get(4),
             clinic_name:get(5),
@@ -50,17 +58,16 @@ impl DentistRow{
             clinic_province:get(8),
             clinic_region:get(9),
             clinic_zip: get(10),
-            clinic_contact:get(11),
+            clinic_contact_numbers:get(11),
             clinic_schedule:get(12),
             dentist_position:get(13),
+            clinic_email: get(15),
         };
-
         Some(dentist)
-
     }
 }
 impl Migration{
-    fn insert_dentists_and_clinics_from_xlsx(_manager: &SchemaManager) -> Result<(), DbErr> {
+    async fn insert_dentists_and_clinics_from_xlsx(manager: &SchemaManager<'_>) -> Result<(), DbErr> {
         static DENTISTS_2026: &[u8] = include_bytes!("../../data_input/dentists_2026.xlsx");
 
         let mut workbook:Xlsx<_> = open_workbook_from_rs(Cursor::new(DENTISTS_2026))
@@ -82,9 +89,66 @@ impl Migration{
             .skip(1)
             .filter_map(DentistRow::from_row)
             .collect();
-        println!("{:?}", dentists);
+
+         for (i,dentist) in dentists.iter().enumerate() {
+             if (i+1)%50==0{
+                 println!("inserted {}/{} dentists",i+1, dentists.len());
+             }
+             Self::insert_dentist_row(&dentist, manager).await?;
+         }
 
 
+        Ok(())
+    }
+
+    async fn insert_dentist_row(d_row: &DentistRow, manager: &SchemaManager<'_>) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        // ---- 1) REGION ----
+        let region_name = clean(&d_row.clinic_region);
+        let region_id = get_or_insert_region(db, &region_name).await?;
+        println!("region_id={region_id}");
+
+        // ---- 2) PROVINCE (unique by name+region_id) ----
+        let province_name = clean(&d_row.clinic_province);
+        let province_id = get_or_insert_province(db, &province_name, region_id).await?;
+        println!("province_id={province_id}");
+
+        // ---- 3) CITY (unique by name+province_id) ----
+        let city_name = clean(&d_row.clinic_city);
+        let city_id = get_or_insert_city(db, &city_name, province_id).await?;
+        println!("city_id={city_id}");
+
+        //------4) DENTIST CONTRACT ----
+        let dentist_contract_name = clean(&d_row.contract);
+        let dentist_contract_id = get_or_insert_dentist_contract(db, &dentist_contract_name).await?;
+        println!("dentist_contract_id={dentist_contract_id}");
+
+        //-----5) DENTAL CLINIC ----
+        let clinic_name = clean(&d_row.clinic_name);
+        let clinic_address = clean(&d_row.clinic_address);
+        let clinic_city_id = Some(city_id);
+        let clinic_zip = Some(d_row.clinic_zip.as_str());
+        let clinic_contact = Some(d_row.clinic_contact_numbers.as_str());
+        let clinic_schedule = Some(d_row.clinic_schedule.as_str());
+        let clinic_email = Some(d_row.clinic_email.as_str());
+
+        let dental_clinic_id = get_or_insert_dental_clinic(
+            db, &clinic_name, &clinic_address, clinic_city_id,
+            clinic_zip,clinic_contact, clinic_schedule, clinic_email
+        ).await?;
+        println!("dental_clinic_id={dental_clinic_id}");
+
+
+        //---6) DENTIST ----
+        let dentist_given_name = clean(&d_row.given_name);
+        let dentist_middle_name = clean(&d_row.middle_name);
+        let dentist_last_name = clean(&d_row.last_name);
+        let dentist_position = clean(&d_row.dentist_position);
+
+        insert_dentist(db, &dentist_given_name, &dentist_middle_name, &dentist_last_name,
+                       dentist_contract_id, dental_clinic_id, &dentist_position).await?;
+        println!("dentist inserted:{dentist_given_name} {dentist_middle_name} {dentist_last_name}");
         Ok(())
     }
 }
@@ -100,4 +164,205 @@ fn cell_to_string(cell:&Data) ->String{
         Data::DateTimeIso(s)=>s.clone(),
         Data::Error(e) => e.to_string(),
     }
+}
+
+// clean trims and normalizes whitespace.
+// it splits a string on the whitespaces then joins them back together with a single space.
+fn clean(s: &str) -> String {
+    s.split_whitespace().collect::<Vec<&str>>().join(" ").trim().to_string()
+}
+
+async fn get_or_insert_region(db: &impl ConnectionTrait, name: &str) -> Result<i32, DbErr> {
+    let insert = Query::insert()
+        .into_table(Region::Table)
+        .columns([Region::Name])
+        .values_panic([name.into()])
+        .on_conflict(OnConflict::column(Region::Name).do_nothing().to_owned())
+        .to_owned();
+
+        db.execute(&insert).await?;
+    println!("inserted region={name}");
+
+    let select = Query::select()
+        .column(Region::Id)
+        .from(Region::Table)
+        .and_where(Expr::col(Region::Name).eq(name))
+        .limit(1)
+        .to_owned();
+
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get region id for {name}")))?;
+
+    Ok(row.try_get("", "id")?)
+}
+
+async fn get_or_insert_province(db: &impl ConnectionTrait, name: &str, region_id: i32) -> Result<i32, DbErr> {
+    let insert = Query::insert()
+        .into_table(Province::Table)
+        .columns([Province::Name, Province::RegionId])
+        .values_panic([name.into(), region_id.into()])
+        .on_conflict(OnConflict::columns([Province::Name, Province::RegionId]).do_nothing().to_owned())
+        .to_owned();
+
+    db.execute(&insert).await?;
+    println!("inserted province={name}");
+
+    let select = Query::select()
+        .column(Province::Id)
+        .from(Province::Table)
+        .and_where(Expr::col(Province::Name).eq(name))
+        .and_where(Expr::col(Province::RegionId).eq(region_id))
+        .limit(1)
+        .to_owned();
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get province id for {name}")))?;
+    Ok(row.try_get("", "id")?)
+}
+
+async fn get_or_insert_city(db: &impl ConnectionTrait, name: &str, province_id: i32) -> Result<i32, DbErr> {
+
+    let insert = Query::insert()
+        .into_table(City::Table)
+        .columns([City::Name, City::ProvinceId])
+        .values_panic([name.into(), province_id.into()])
+        .on_conflict(OnConflict::columns([City::Name, City::ProvinceId]).do_nothing().to_owned())
+        .to_owned();
+    db.execute(&insert).await?;
+    println!("inserted city={name}");
+
+    let select = Query::select()
+        .column(City::Id)
+        .from(City::Table)
+        .and_where(Expr::col(City::Name).eq(name))
+        .and_where(Expr::col(City::ProvinceId).eq(province_id))
+        .limit(1)
+    .to_owned();
+
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get city id for {name}")))?;
+    Ok(row.try_get("", "id")?)
+}
+
+
+async fn get_or_insert_dentist_contract(db: &impl ConnectionTrait, name: &str) -> Result<i32, DbErr> {
+    let insert = Query::insert()
+        .into_table(DentistContract::Table)
+        .columns([DentistContract::Name, DentistContract::LastModifiedBy])
+        .values_panic([name.into(), "system".into()])
+        .on_conflict(OnConflict::column(DentistContract::Name).do_nothing().to_owned())
+        .to_owned();
+
+    db.execute(&insert).await?;
+    println!("inserted dentist_contract={name}");
+
+    let select = Query::select()
+        .column(DentistContract::Id)
+        .from(DentistContract::Table)
+        .and_where(Expr::col(DentistContract::Name).eq(name))
+        .limit(1)
+        .to_owned();
+
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get dentist_contract id for {name}")))?;
+
+    Ok(row.try_get("", "id")?)
+
+}
+
+async fn get_or_insert_dental_clinic(db: &impl ConnectionTrait,
+                                     clinic_name: &str, clinic_address: &str,
+                                     clinic_city_id:Option<i32>, clinic_zip: Option<&str>,
+                                     clinic_contact: Option<&str>, clinic_schedule: Option<&str>,
+                                     clinic_email: Option<&str>
+) -> Result<i32, DbErr> {
+    let insert = Query::insert()
+        .into_table(DentalClinic::Table)
+        .columns([DentalClinic::Name,
+            DentalClinic::Address,
+            DentalClinic::CityId,
+            DentalClinic::ZipCode,
+            DentalClinic::ContactNumbers,
+            DentalClinic::Schedule,
+            DentalClinic::Email
+
+        ])
+        .values_panic([
+            clinic_name.into(),
+            clinic_address.into(),
+            clinic_city_id.into(),
+            clinic_zip.into(),
+            clinic_contact.into(),
+            clinic_schedule.into(),
+            clinic_email.into()])
+        .on_conflict(OnConflict::columns([
+            DentalClinic::Name,
+            DentalClinic::Address,
+            DentalClinic::CityId,
+            DentalClinic::ZipCode])
+            .do_nothing()
+            .to_owned()
+        )
+        .to_owned();
+
+    db.execute(&insert).await?;
+    println!("inserted dental_clinic={clinic_name}");
+
+    let select = Query::select()
+        .column(DentalClinic::Id)
+        .from(DentalClinic::Table)
+        .and_where(Expr::col(DentalClinic::Name).eq(clinic_name))
+        .and_where(Expr::col(DentalClinic::Address).eq(clinic_address))
+        .and_where(Expr::col(DentalClinic::CityId).eq(clinic_city_id))
+        .and_where(Expr::col(DentalClinic::ZipCode).eq(clinic_zip))
+        .limit(1)
+        .to_owned();
+
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get dental_clinic id for {clinic_name}")))?;
+
+    Ok(row.try_get("", "id")?)
+}
+
+
+async fn insert_dentist(db: &impl ConnectionTrait,
+                        given_name:&str, middle_name: &str, last_name: &str,
+                        dental_contract_id: i32, dental_clinic_id: i32, position: &str) -> Result<(), DbErr> {
+    let insert = Query::insert()
+        .into_table(Dentist::Table)
+        .columns([Dentist::AccreDentistContractId,
+        Dentist::GivenName,
+        Dentist::MiddleName,
+        Dentist::LastName,
+        ])
+        .values_panic([dental_contract_id.into(),
+                            given_name.into(),
+                            middle_name.into(),
+                            last_name.into(),
+                            ])
+        .to_owned();
+    db.execute(&insert).await?;
+
+    let select = Query::select()
+        .from(Dentist::Table)
+        .column(Dentist::Id)
+        .and_where(Expr::col(Dentist::GivenName).eq(given_name))
+        .and_where(Expr::col(Dentist::MiddleName).eq(middle_name))
+        .and_where(Expr::col(Dentist::LastName).eq(last_name))
+        .limit(1)
+        .to_owned();
+
+    let row = db.query_one(&select).await?
+        .ok_or_else(|| DbErr::Custom(format!("Failed to get dentist id for {given_name} {middle_name} {last_name}")))?;
+
+   let dentist_id: i32 = row.try_get("", "id")?;
+
+   let insert_dentist_clinic = Query::insert()
+       .into_table(DentistClinic::Table)
+       .columns([DentistClinic::DentistId, DentistClinic::ClinicId, DentistClinic::Position])
+       .values_panic([dentist_id.into(), dental_clinic_id.into(), position.into()])
+       .to_owned();
+
+    db.execute(&insert_dentist_clinic).await?;
+
+    Ok(())
 }
