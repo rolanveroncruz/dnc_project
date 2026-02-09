3b62af4d78a2de690346d2efd5ce8cbf112ac907
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-02-09 22:00:35 +0800

    edited otelTracesUrl in environment.production.ts.

--- dnc_frontend/src/environments/environment.production.ts
+++ dnc_frontend/src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiUrl: '/api',
     // same-origin path that your reverse proxy will forward to the collector
-    otelTracesUrl: '/otlp/v1/traces',
+    otelTracesUrl: `${window.location.origin}/otlp/v1/traces`,
     apiBaseUrl: '/api',
 };
