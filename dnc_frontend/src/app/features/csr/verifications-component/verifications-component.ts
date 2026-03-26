bb4c9e67c99929242fddfd6ba89b424920230ae3
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-03-27 00:57:58 +0800

    verifications frontend good

--- dnc_frontend/src/app/features/csr/verifications-component/verifications-component.ts
+++ dnc_frontend/src/app/features/csr/verifications-component/verifications-component.ts
@@ -28,22 +28,53 @@ export class VerificationsComponent implements OnInit {
     private readonly destroyRef = inject(DestroyRef);
     verifications = signal<VerificationLookupResponse[]>([]);
 
+    getLabelFromRow(row: VerificationLookupResponse): string {
+        return row.status_name;
+    }
     readonly columns: TableColumn<VerificationLookupResponse>[] = [
         { key: 'verification_id', label: 'ID' },
         { key: 'date_created', label: 'Date', cellTemplateKey: 'date' },
         { key: 'dentist_name', label: 'Dentist'},
         { key: 'master_list_member_name', label: 'Member'},
         {key: 'dental_service_name', label: 'Service'},
+        {key: 'status_name', label: 'Status'},
+        { key: 'actions', label:'Actions', sortable: false, cellTemplateKey: 'actions',
+            actionButton: {
+                label: this.getRowLabel,
+                icon: this.getRowIcon,
+                color: 'primary',
+                onClick: function (row: VerificationLookupResponse): void {
+                    console.log("In onActionButtonClicked(), row:", row);
+                }
+            },
+        }
     ]
+    getRowLabel(row: VerificationLookupResponse): string {
+        return row.status_id==1 ? 'Approval Code' : 'Upload Files';
+    }
+
+    getRowIcon(row: VerificationLookupResponse): string {
+        return row.status_id==1 ? 'check' : 'upload';
+    }
+
+    onActionButtonClicked(row:VerificationLookupResponse): void {
+        console.log("In onActionButtonClicked(), row:", row);
+    }
+
 
     ngOnInit(): void {
+        this.loadVerifications();
 
+    }
+
+    loadVerifications(){
         this.verificationService.getVerifications()
             .pipe(takeUntilDestroyed(this.destroyRef))
             .subscribe({
                 next:(res)=> this.verifications.set(res),
                 error:(err)=> console.log("In load(), failed to load verifications",err )
             })
+
     }
 
     onNewVerification(){
@@ -56,4 +87,10 @@ export class VerificationsComponent implements OnInit {
         // Route example: /setup/dental-clinics/:id
         this.router.navigate(['main/csr/verifications/', row.verification_id]).then();
     }
+
+    onCancelVerification(row: VerificationLookupResponse){
+        console.log("In onCancelVerification()", row);
+
+    }
+
 }
